c2a184b7aad83309373613576be926c141fb677b
GustavoAssi <alencarassigustavo@gmail.com>
2021-03-18 20:10:51 +0000

    Parte 1 Python HTML e CSS

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Gustavo'}
+    posts = [
+        {'author': {'username': 'Maria'}, 'body': "Olá da Maria"},
+        {'author': {'username': 'Feulo'}, 'body': "Olá!"}
+    ]
+    return render_template("index.html", user=user, posts=posts)
